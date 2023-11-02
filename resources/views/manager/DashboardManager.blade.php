5e719c2e7fe49447e0927fc8074d3c56c71860ce
reza.muhamad <reza.muhamad@pactindo.com>
2023-11-02 17:01:02 +0700

    dashboard manager

--- resources/views/manager/DashboardManager.blade.php
+++ resources/views/manager/DashboardManager.blade.php
@@ -35,6 +35,71 @@ Dashboard Manager
                 </div>
               </div>
             </div>
+            <div class="col">
+                <canvas id="donutChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+            </div>
           </div>
 </section>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="{{asset('assets/chart.js/Chart.min.js'); }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@3.6.0"></script>
+<script>
+  $(function() {
+    /* ChartJS
+     * -------
+     * Here we will create a few charts using ChartJS
+     */
+
+
+
+    //-------------
+    //- DONUT CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+    var akunData = {
+      labels: [
+        'Total Data Laporan',
+        'Total Data Produk'
+      ],
+      datasets: [{
+        data: [
+          1,
+          2,
+        ],
+        backgroundColor: ['#fc544b', '#6777ef'],
+      }]
+    }
+    var donutOptions = {
+      maintainAspectRatio: false,
+      responsive: true,
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    new Chart(donutChartCanvas, {
+      type: 'doughnut',
+      data: akunData,
+      options: donutOptions
+    })
+
+    //-------------
+    //- PIE CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+    var pieData = proyekData;
+    var pieOptions = {
+      maintainAspectRatio: false,
+      responsive: true,
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    new Chart(pieChartCanvas, {
+      type: 'pie',
+      data: pieData,
+      options: pieOptions
+    })
+
+  })
+</script>
 @endsection
